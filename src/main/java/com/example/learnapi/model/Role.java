9d4bf8183cfc2e178ed03a9f7c42b88e8c6987d4
RidhoUdev <muhammadridhouu@gmail.com>
2025-07-21 10:27:52 +0700

    feat: implement authentication api

---
+++ src/main/java/com/example/learnapi/model/Role.java
@@ -0,0 +1,6 @@
+package com.example.learnapi.model;
+
+public enum Role {
+    USER,
+    ADMIN,
+}
---
+++ src/main/java/com/example/learnapi/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.learnapi.repository;
+
+import com.example.learnapi.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+    Optional<User> findByUsername(String username);
+}
